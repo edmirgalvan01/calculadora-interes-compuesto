5020240302e001bf3f063d3ab231252a1d33e218
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-26 12:20:59 -0600

    Se muestra el balance correctamenet con formato en USD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { Formik, Form } from 'formik';
 import Input from './components/Input';
 import Button from './components/Button';
 import Container from './components/Container';
 import Section from './components/Section';
+import Balance from './components/Balance';
 
 const compoundInterest = (deposit, contribution, years, rate) => {
    let total = deposit;
@@ -12,7 +14,16 @@ const compoundInterest = (deposit, contribution, years, rate) => {
    return Math.round(total);
 };
 
+const formatter = new Intl.NumberFormat('en-US', {
+   style: 'currency',
+   currency: 'USD',
+   minimumFractionDigits: 2,
+   maximumFractionDigits: 2,
+});
+
 function App() {
+   const [balance, setBalance] = useState('');
+
    const handleSubmit = ({ deposit, contribution, years, rate }) => {
       const val = compoundInterest(
          Number(deposit),
@@ -20,7 +31,7 @@ function App() {
          Number(years),
          Number(rate)
       );
-      console.log(val);
+      setBalance(formatter.format(val));
    };
 
    return (
@@ -43,6 +54,9 @@ function App() {
                   <Button>Calcular</Button>
                </Form>
             </Formik>
+            {balance !== '' ? (
+               <Balance>Balance final: {balance}</Balance>
+            ) : null}
          </Section>
       </Container>
    );
