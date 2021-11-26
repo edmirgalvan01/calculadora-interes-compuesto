bef4c2477be41314f3d7f2e827e4391f9e51e825
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-26 11:25:53 -0600

    Creamos nuestro componente de Input que contiene label e input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,33 @@
 import styled from 'styled-components';
 import { useField } from 'formik';
 
+const Control = styled.div`
+   margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+   color: #000;
+   display: block;
+   margin-bottom: 5px;
+`;
+
+const MyInput = styled.input`
+   outline: none;
+   padding: 8px;
+   border: 1px solid #b1b3b5;
+   border-radius: 4px;
+   width: 100%;
+   margin-bottom: 5px;
+`;
+
 const Input = ({ label, ...props }) => {
-   return 'lala';
+   const [field, meta] = useField(props);
+   return (
+      <Control>
+         <Label>{label}</Label>
+         <MyInput {...field} {...props} />
+      </Control>
+   );
 };
 
 export default Input;
